e9a3b593bf00e37e0e4fff8f91b5bb60952ce4a9
AbdulRahmanPro <progammerabood@gmail.com>
2023-07-28 13:46:29 +0300

    توقف عند القسم الثاني وتأكد من عمله ؟

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  content: ['./dist/**/*.html', './**/*.js'], // قم بتحديد جميع ملفات HTML و JS الموجودة في المشروع
+  content: ["./dist/**/*.html", "./**/*.js"], // قم بتحديد جميع ملفات HTML و JS الموجودة في المشروع
   theme: {
-
-    extend: {}
+    extend: {
+      colors: {
+        "primary-color": "#00ADB5",
+        "Text_color_primary":"#EEEEEE",
+        "Dark_color_primary":"#222831"
+      },
+      boxShadow: {
+        'text-shadow': 'text-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.50);',
+      },
+    },    
   },
   plugins: [],
-}
+};
